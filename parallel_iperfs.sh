804840c3eb69990bbebac47d790b3bd2a359ba13
lakshman kumar <lakshmankumar@gmail.com>
2021-05-19 12:31:27 +0530

    support listfile in parallel iperf

--- parallel_iperfs.sh
+++ parallel_iperfs.sh
@@ -7,9 +7,12 @@ fileprefix=parallel_iperf
 rate=2M
 parallel=1
 prot="tcp"
+listfile=""
 
 function usage() {
-  echo "$0 (--server|--client) [--rate <rate:as-is-to-iperf-client,eg:10M>] [--parallel <n:as-is-Parg,eg:3] [--udp|--tcp(default)]"
+  echo "$0 (--server|--client) [--rate <rate:as-is-to-iperf-client,eg:10M>] [--parallel <n:as-is-Parg,eg:3] [--udp|--tcp(default)] [--listfile <file>]"
+  echo "   --listfile should have values in this fashion per-line:"
+  echo "            port=<port> rate=<rate> prot=tcp|udp parallel=1"
   exit 1
 }
 
@@ -36,10 +39,12 @@ while [[ $# > 0  ]] ; do
       ;;
     --tcp)
       prot="tcp"
-      shift 1
       ;;
     --udp)
       prot="udp"
+      ;;
+    --listfile)
+      listfile=$1
       shift 1
       ;;
     *)
@@ -49,25 +54,59 @@ while [[ $# > 0  ]] ; do
   esac
 done
 
-
-protarg=""
-if [ $prot = "udp" ] ; then
-  protarg="-u"
-fi
-for i in $(seq 1 $count) ; do
-  port=$((${start_port} + ${i}))
-  if [[ $server = 1 ]] ; then
-    cmd="./iperf3 -s -B ${mip} -p ${port} --logfile ${fileprefix}_${i}_server &"
-  else
-    cmd="./iperf3 -B ${mip} -p ${port} -c ${pip} -i 1 -t 3600 -b${rate} -P${parallel} --logfile ${fileprefix}_${i}_client ${protarg} &"
-  fi
-  echo "doing ${cmd}"
-  eval $cmd
-  if [ $? -ne 0 ] ; then
-    echo "Not successful.."
+if [ -n "$listfile" ] ; then
+  if [ ! -f "$listfile" ] ; then
+    echo "Couldn't locate $listfile"
     exit 1
   fi
-done
+  i=0
+  while read line ; do
+    i=$((i+1))
+    port=0
+    rate=0
+    prot="tcp"
+    parallel=1
+    protarg=""
+    eval $line
+    if [ $port == 0 ] ; then
+      echo "port:$port is 0 in line:$line"
+      exit 1
+    fi
+    if [ $rate == 0 ] ; then
+      echo "rate:$rate is 0 in line:$line"
+      exit 1
+    fi
+    if [ "$prot" = "udp" ] ; then
+      protarg="-u"
+    fi
+    cmd="./iperf3 -B ${mip} -p ${port} -c ${pip} -i 1 -t 3600 -b${rate} -P${parallel} --logfile ${fileprefix}_${i}_client ${protarg} &"
+    echo "doing ${cmd}"
+    eval $cmd
+    if [ $? -ne 0 ] ; then
+      echo "Not successful.."
+      exit 1
+    fi
+  done < $listfile
+else
+  protarg=""
+  if [ $prot = "udp" ] ; then
+    protarg="-u"
+  fi
+  for i in $(seq 1 $count) ; do
+    port=$((${start_port} + ${i}))
+    if [[ $server = 1 ]] ; then
+      cmd="./iperf3 -s -B ${mip} -p ${port} --logfile ${fileprefix}_${i}_server &"
+    else
+      cmd="./iperf3 -B ${mip} -p ${port} -c ${pip} -i 1 -t 3600 -b${rate} -P${parallel} --logfile ${fileprefix}_${i}_client ${protarg} &"
+    fi
+    echo "doing ${cmd}"
+    eval $cmd
+    if [ $? -ne 0 ] ; then
+      echo "Not successful.."
+      exit 1
+    fi
+  done
+fi
 
 echo "Started and waiting"
 wait
